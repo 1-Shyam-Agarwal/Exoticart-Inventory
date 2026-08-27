f8b5479ca64f84b2bc6fb6ce3b6631bca4225577
Shyam Agarwal <shyam.agarwal.profess@gmail.com>
2026-08-27 13:49:34 +0530

    fix: prisma-modek

---
+++ prisma/migrations/20260827134815_drop_unit_dimension_weight_tables/migration.sql
@@ -0,0 +1,15 @@
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "DimensionUnit";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Unit";
+PRAGMA foreign_keys=on;
+
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "WeightUnit";
+PRAGMA foreign_keys=on;
+
